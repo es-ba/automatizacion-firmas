ab24f96f85a1a6a5f4d628eb4070bee64d970d2a
EliasGantus <elias.gantus@gmail.com>
2024-03-27 15:38:10 -0300

    bug fix

--- src/server/procedures-firmas.ts
+++ src/server/procedures-firmas.ts
@@ -9,13 +9,14 @@ export const ProceduresFirmas: ProcedureDef[] = [
         action: 'firmas_generar',
         parameters: [],
         coreFunction: async (context: ProcedureContext, _params: CoreFunctionParameters) => {
-            const modelo = await context.client.query(`
+            await context.client.query(`
                 UPDATE personal p
                 SET firma_generada_html = 
                 REPLACE(mf.template_html,'$$telefono',COALESCE(p.telefono::text,''))
                 FROM modelos_firma mf
                 where p.modelo=mf.modelo;
             `).execute()
+            
             return 'ok'
         }
     }
