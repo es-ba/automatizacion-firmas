39e584274d2097670598c1f1f5953dece2cd7e14
manueldelapenna <manueldelapenna@gmail.com>
2024-04-03 15:59:20 -0300

    cambio funcion recursiva por reduce, que además funciona si el array está vacío

--- src/server/procedures-firmas.ts
+++ src/server/procedures-firmas.ts
@@ -9,25 +9,20 @@ export const ProceduresFirmas: ProcedureDef[] = [
         action: 'firmas_generar',
         parameters: [],
         coreFunction: async (context: ProcedureContext, _params: CoreFunctionParameters) => {
-            function replaceField(replaceList: {target:string, source:string}[]): string{
-                const currentElem = replaceList.pop()
-                return `REPLACE(${replaceList.length==0? 'mf.template_html': replaceField(replaceList)}, '$$${currentElem!.target}',COALESCE(p.${currentElem!.source}::text,''))`
-            }
-            
             let fieldsToReplace: {target:string, source:string}[] = [
                 {target:'direccion_postal1', source:'dir_postal1'}, 
                 {target:'telefono', source:'telefono'},
                 {target:'direccion_postal2', source:'dir_postal2'}
             ];
-
+            let replacers = fieldsToReplace.reduce((query , currentElem, currentIndex )=>{
+                return `REPLACE(${currentIndex==0? 'mf.template_html': query}, '$$${currentElem.target}',COALESCE(p.${currentElem.source}::text,''))`
+            },`''`)
             await context.client.query(`
                 UPDATE personal p
-                    SET firma_generada_html = 
-                    ${replaceField(fieldsToReplace)}
+                    SET firma_generada_html = nullif(${replacers},'')
                     FROM modelos_firma mf
                     where p.modelo=mf.modelo;
             `).execute()
-
             return 'ok'
         }
     }
