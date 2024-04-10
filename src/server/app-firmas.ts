1fd127af15cae981349bb44c879a08b5cadb3347
manueldelapenna <manueldelapenna@gmail.com>
2024-04-10 14:11:47 -0300

    descarga de firma

--- src/server/app-firmas.ts
+++ src/server/app-firmas.ts
@@ -5,6 +5,8 @@ import {modelos_firma} from "./table-modelos_firma"
 import { personal } from "./table-personal";
 import { ProceduresFirmas } from "./procedures-firmas";
 import * as miniTools from "mini-tools";
+import * as stream from "stream";
+
 export class AppFirmas extends AppBackend {
 
     configStaticConfig(){
@@ -16,30 +18,46 @@ export class AppFirmas extends AppBackend {
         let be=this;
         super.addSchrödingerServices(mainApp, baseUrl);
         
+        const getHtml = async (be, req)=>
+            await be.inDbClient(req, async function(client){
+                if(!req.query.cuit){
+                    throw Error (`no se ingresó cuit en parámetro` )
+                }
+                const html = (await client.query(`
+                    select firma_generada_html 
+                        from personal 
+                        where cuit = $1`,
+                    [req.query.cuit]).fetchUniqueValue()).value;
+                if(!html){
+                    throw Error (`no hay firma generada para el cuit ${req.query.cuit}` )
+                }
+                return html;
+            });
         
         mainApp.get(baseUrl+'/firma-mostrar',async function(req,res,next){
-            // @ts-ignore sé que voy a recibir useragent por los middlewares de Backend-plus
-            var {useragent} = req;
-            await be.inDbClient(req, async function(client){
-                try{
-                    if(!req.query.cuit){
-                        throw Error (`no se ingresó cuit en parámetro` )
-                    }
-                    const html = (await client.query(`
-                        select firma_generada_html 
-                            from personal 
-                            where cuit = $1`,
-                        [req.query.cuit]).fetchUniqueValue()).value;
-                    if(!html){
-                        throw Error (`no hay firma generada para el cuit ${req.query.cuit}` )
-                    }
-                    
-                    miniTools.serveText(html, 'html')(req,res);
-                }catch(err){
-                    console.log(err);
-                    miniTools.serveErr(req, res, next)(err);
-                }
-            })
+            try{
+                const html = await getHtml(be,req);
+                miniTools.serveText(html, 'html')(req,res);
+            }catch(err){
+                console.log(err);
+                miniTools.serveErr(req, res, next)(err);
+            }
+        });
+        mainApp.get(baseUrl+'/firma-descargar',async function(req,res,next){
+            try{
+                const fileData = await getHtml(be,req);
+                const fileName = 'firma.html'
+                const fileType = 'text/html'
+                res.writeHead(200, {
+                    'Content-Disposition': `attachment; filename="${fileName}"`,
+                    'Content-Type': fileType,
+                })
+                const download = Buffer.from(fileData)
+                res.end(download)
+            }catch(err){
+                console.log(err);
+                miniTools.serveErr(req, res, next)(err);
+            }
         });
     }
 
