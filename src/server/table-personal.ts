6f75e64e73878d4a46ce7441f5dffb2a6c14e56b
Sebastián <spierini@info.lifia.unlp.edu.ar>
2024-04-30 14:28:37 -0300

    se modifica posición de columna acciones

--- src/server/table-personal.ts
+++ src/server/table-personal.ts
@@ -9,10 +9,10 @@ export function personal(context: TableContext): TableDefinition {
         title: 'Personal',
         editable: admin,
         fields: [
-            { name: 'acciones',             inTable:false, typeName:'text'      , editable:false  , clientSide:'accionesFirma'},
             { name: 'cuit', typeName: 'bigint', nullable: false },
             { name: 'modelo', typeName: 'text', nullable: false },
             { name: 'titular', typeName: 'text', nullable: false },
+            { name: 'acciones',             inTable:false, typeName:'text'      , editable:false  , clientSide:'accionesFirma'},
             { name: 'division', typeName: 'text' },
             { name: 'departamento', typeName: 'text' },
             { name: 'direccion', typeName: 'text' },
